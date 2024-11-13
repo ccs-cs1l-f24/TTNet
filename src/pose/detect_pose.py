b99a27d40bcfbb73d29c721294e648b805826add
Dylan Lu <dylanelu@gmail.com>
2024-11-13 08:02:13 -0800

    video to json

--- src/pose/detect_pose.py
+++ src/pose/detect_pose.py
@@ -2,6 +2,7 @@ import mediapipe as mp
 import cv2
 import numpy as np
 import json
+import os
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 from mediapipe.framework.formats import landmark_pb2
@@ -11,19 +12,43 @@ PoseLandmarker = mp.tasks.vision.PoseLandmarker
 PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+MP_MODEL_PATH = os.path.join(CURRENT_DIR, "pose_landmarker_heavy.task")
+
 # Create a pose landmarker instance with the video mode:
 options = PoseLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path="pose_landmarker_heavy.task"),
+    base_options=BaseOptions(model_asset_path=MP_MODEL_PATH),
     running_mode=VisionRunningMode.VIDEO,
     min_pose_detection_confidence=0.8,
     min_pose_presence_confidence=0.8,
     min_tracking_confidence=0.8
 )
 
-def detect_pose(img):
+def detect_pose(img: np.ndarray, blackout_left: bool, frame_timestamp_ms: int):
     """ Detect human pose on an image
-    :param img: (1920, 1080)
+    :param img: (1080, 1920, 3) in RGB
+    :param blackout_left: Should the left side be blacked out (otherwise right is blacked out)
+    :param frame_timestamp_ms: The timestamp to the nearest millisecond
     :return: array of landmarks (which are relative to the hips)
     """
 
-    pass
+    height, width, channels = img.shape
+
+    with PoseLandmarker.create_from_options(options) as landmarker:
+        if blackout_left:
+            img[:, :width//2] = 0
+        else:
+            img[:, width//2:] = 0
+
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        pose_result = landmarker.detect_for_video(mp_image, frame_timestamp_ms)
+
+        if pose_result.pose_world_landmarks:
+            world_landmarks = [{"name": f"landmark_{i}", "x": landmark.x, "y": landmark.y, "z": landmark.z, "visibility": landmark.visibility}
+                                for i, landmark in enumerate(pose_result.pose_world_landmarks[0])]
+
+            return world_landmarks
+
+    print("ERROR: No landmarks detected")
+    return []
+        
--- src/video_to_json.py
+++ src/video_to_json.py
@@ -5,25 +5,73 @@ import cv2
 import numpy as np
 import torch
 import time
+import json
 
 sys.path.append('./')
 
-from data_process.ttnet_video_loader import TTNet_Video_Loader
+from data_process.ttnet_video_loader_v2 import TTNet_Video_Loader_V2
 from models.model_utils import create_model, load_pretrained_model
 from config.config import parse_configs
 from utils.post_processing import post_processing
 from utils.misc import time_synchronized
+from pose.detect_pose import detect_pose
 
-def process_video():
-
-    # Load the video
+def process_video(configs):
+    # Load the video and model
+    video_loader = TTNet_Video_Loader_V2(configs.video_path, configs.input_size, configs.num_frames_sequence)
+    frame_rate = video_loader.video_fps
+    configs.device = torch.device("cuda:{}".format(configs.gpu_idx))
+    model = create_model(configs)
+    model.cuda()
+    model = load_pretrained_model(model, configs.pretrained_path, configs.gpu_idx, configs.overwrite_global_2_local)
+    model.eval()
+    middle_idx = int(configs.num_frames_sequence / 2)
+    queue_frames = deque(maxlen=middle_idx + 1)
+    frame_idx = 0
+    w_original, h_original = 1920, 1080
+    w_resize, h_resize = 320, 128
+    w_ratio = w_original / w_resize
+    h_ratio = h_original / h_resize
 
     # For each frame
+    with torch.no_grad():
+        output_json_data = {
+            "img_size": [w_original, h_original],
+            "frames": []
+        }
 
-    # Get ball position
-    # Detect whether it bounced => Find position relative to table
+        for frame_cnt, frame_timestamp_ms, resized_imgs, orig_imgs in video_loader:
+            img = orig_imgs[3 * middle_idx: 3 * (middle_idx + 1)].transpose(1, 2, 0)
+            resized_imgs = torch.from_numpy(resized_imgs).to(configs.device, non_blocking=True).float().unsqueeze(0)
 
-    # Get the landmarks for left and right
+            # Get ball position and whether it bounced
+            t1 = time_synchronized()
+            pred_ball_global, pred_ball_local, pred_events, pred_seg = model.run_demo(resized_imgs)
+            t2 = time_synchronized()
+            prediction_global, prediction_local, prediction_seg, prediction_events = post_processing(
+                pred_ball_global, pred_ball_local, pred_events, pred_seg, configs.input_size[0],
+                configs.thresh_ball_pos_mask, configs.seg_thresh, configs.event_thresh)
+            prediction_ball_final = [
+                int(prediction_global[0] * w_ratio + prediction_local[0] - w_resize / 2),
+                int(prediction_global[1] * h_ratio + prediction_local[1] - h_resize / 2)
+            ]
+
+            # If it bounced, find position relative to table
+
+            # Get the landmarks for left and right
+            right_world_landmarks = detect_pose(img=img, blackout_left=True, frame_timestamp_ms=frame_timestamp_ms)
+            left_world_landmarks = detect_pose(img=img, blackout_left=False, frame_timestamp_ms=frame_timestamp_ms)
+
+            frame_data = {
+                "frame": frame_cnt,
+                "right_landmarks": right_world_landmarks,
+                "left_landmarks": left_world_landmarks,
+                "ball_pos": prediction_ball_final
+            }
+            output_json_data["frames"].append(frame_data)
+    
+        with open("../results/output_data.json", "w") as json_file:
+            json.dump(output_json_data, json_file, indent=2)
 
     # Put in json array
     """
@@ -62,3 +110,5 @@ if __name__ == '__main__':
     configs.show_image = False
     configs.save_demo_output = True
     configs.no_seg = True
+
+    process_video(configs=configs)
